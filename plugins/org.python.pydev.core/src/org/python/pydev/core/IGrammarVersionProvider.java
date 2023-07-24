69a60a12d8812e3fe9ff8002fc478a32a034fc34
KimKyengJun <juniair.kim@gmail.com>
2023-07-24 11:51:15 +0900

    Squashed commit of the following:
    commit 2b2fdac8924f7527b57f224c3a622280c2d0e201
    Author: KimKyengJun <juniair.kim@gmail.com>
    Date:   Mon Jul 24 11:20:48 2023 +0900
    
        [Revert] Added Python 2.x grammar
    
    commit 3015123fbeac3a688c85f536addb7149d034759d
    Author: KimKyengJun <juniair.kim@gmail.com>
    Date:   Mon Jul 24 11:05:15 2023 +0900
    
        [Revert] Python 2.x Build 복원

--- plugins/org.python.pydev.core/src/org/python/pydev/core/IGrammarVersionProvider.java
+++ plugins/org.python.pydev.core/src/org/python/pydev/core/IGrammarVersionProvider.java
@@ -19,6 +19,21 @@ import java.util.Map;
 import java.util.Set;
 
 public interface IGrammarVersionProvider {
+
+    /**
+     * Constants for knowing the version of a grammar (so that jython 2.1 and python 2.1 can be regarded
+     * as GRAMMAR_PYTHON_VERSION_2_1), so, in short, it does not differentiate among the many flavors of python
+     *
+     * They don't start at 0 because we don't want any accidents... ;-)
+     */
+    public static final int GRAMMAR_PYTHON_VERSION_2_5 = 11;
+    public static final int GRAMMAR_PYTHON_VERSION_2_6 = 12;
+    public static final int GRAMMAR_PYTHON_VERSION_2_7 = 13;
+    public static final int LATEST_GRAMMAR_PY2_VERSION = GRAMMAR_PYTHON_VERSION_2_7;
+
+    /**
+     * Just in case you're wondering, Version 3 is not the latest... it's as if it's a new grammar entirely.
+     */
     public static final int GRAMMAR_PYTHON_VERSION_3_5 = 99;
     public static final int GRAMMAR_PYTHON_VERSION_3_6 = 100;
     public static final int GRAMMAR_PYTHON_VERSION_3_7 = 101;
@@ -27,7 +42,6 @@ public interface IGrammarVersionProvider {
     public static final int GRAMMAR_PYTHON_VERSION_3_10 = 104;
     public static final int GRAMMAR_PYTHON_VERSION_3_11 = 105;
     public static final int LATEST_GRAMMAR_PY3_VERSION = GRAMMAR_PYTHON_VERSION_3_11;
-    public static final int LATEST_GRAMMAR_PY2_VERSION = GRAMMAR_PYTHON_VERSION_3_11;
 
     /**
      * So, no specific reason for the 777 number (just wanted something unique that wouldn't be close to the other grammars).
@@ -72,6 +86,9 @@ class GrammarsIterator {
 
     public static List<Integer> createList() {
         List<Integer> grammarVersions = new ArrayList<>();
+        grammarVersions.add(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_2_5);
+        grammarVersions.add(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_2_6);
+        grammarVersions.add(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_2_7);
         grammarVersions.add(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_3_5);
         grammarVersions.add(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_3_6);
         grammarVersions.add(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_3_7);
@@ -84,6 +101,9 @@ class GrammarsIterator {
 
     public static List<String> createStr() {
         List<String> grammarVersions = new ArrayList<>();
+        grammarVersions.add("2.5");
+        grammarVersions.add("2.6");
+        grammarVersions.add("2.7");
         grammarVersions.add("3.5");
         grammarVersions.add("3.6");
         grammarVersions.add("3.7");
@@ -96,6 +116,9 @@ class GrammarsIterator {
 
     public static Map<Integer, String> createDict() {
         HashMap<Integer, String> ret = new HashMap<>();
+        ret.put(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_2_5, "2.5");
+        ret.put(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_2_6, "2.6");
+        ret.put(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_2_7, "2.7");
         ret.put(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_3_5, "3.5");
         ret.put(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_3_6, "3.6");
         ret.put(IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_3_7, "3.7");
@@ -108,6 +131,9 @@ class GrammarsIterator {
 
     public static Map<String, Integer> createStrToInt() {
         HashMap<String, Integer> ret = new HashMap<>();
+        ret.put("2.5", IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_2_5);
+        ret.put("2.6", IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_2_6);
+        ret.put("2.7", IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_2_7);
         ret.put("3.5", IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_3_5);
         ret.put("3.6", IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_3_6);
         ret.put("3.7", IGrammarVersionProvider.GRAMMAR_PYTHON_VERSION_3_7);
